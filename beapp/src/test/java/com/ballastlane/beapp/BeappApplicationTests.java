931f2a38e30233aa48c23eadde5f5c0d4aeeefc7
Leonardo Mejillones <leonardo.mejillones@gmail.com>
2023-10-21 10:00:58 -0400

    First commit

---
+++ beapp/src/test/java/com/ballastlane/beapp/BeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ballastlane.beapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
