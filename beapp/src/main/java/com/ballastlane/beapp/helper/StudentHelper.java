93a8eeb143eece0238e138a6736a9a5003149af2
Leonardo Mejillones <leonardo.mejillones@gmail.com>
2023-10-21 17:06:56 -0400

    Add StudentHelper

---
+++ beapp/src/main/java/com/ballastlane/beapp/helper/StudentHelper.java
@@ -0,0 +1,18 @@
+package com.ballastlane.beapp.helper;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.sql.Date;
+
+@Service
+public class StudentHelper {
+    public static int MIN_AGE = 16;
+
+    public Boolean isValidAge(Date dob) {
+        LocalDate curDate = LocalDate.now();
+        int ageStudent = Period.between(dob.toLocalDate(), curDate).getYears();
+        return ageStudent < MIN_AGE;
+    }
+}
--- beapp/src/main/java/com/ballastlane/beapp/service/StudentService.java
+++ beapp/src/main/java/com/ballastlane/beapp/service/StudentService.java
@@ -1,30 +1,29 @@
 package com.ballastlane.beapp.service;
 
 import com.ballastlane.beapp.errorhandler.CustomErrorException;
+import com.ballastlane.beapp.helper.StudentHelper;
 import com.ballastlane.beapp.model.Student;
 import com.ballastlane.beapp.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Period;
 
 @Service
 public class StudentService {
-    public static int MIN_AGE = 16;
 
     @Autowired
     private StudentRepository studentRepository;
+
+    @Autowired
+    private StudentHelper studentHelper;
 
     public Student getStudent(Long id) {
         return studentRepository.findById(id).orElse(null);
     }
 
     public Student saveStudent(Student student) throws Exception {
-        LocalDate curDate = LocalDate.now();
-        int ageStudent = Period.between(student.getDob().toLocalDate(), curDate).getYears();
-        if (ageStudent < MIN_AGE) {
+        if (studentHelper.isValidAge(student.getDob())) {
             throw new CustomErrorException(
                     HttpStatus.BAD_REQUEST,
                     "Minimum 16 years old"
@@ -32,4 +31,6 @@ public class StudentService {
         }
         return studentRepository.save(student);
     }
+
+
 }
